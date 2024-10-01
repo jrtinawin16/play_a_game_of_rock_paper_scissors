4f41720bbc6dc1ac450812fc6d0add7052420fc1
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-10-01 17:31:06 +0800

    Accomplished player's prompt to select a choice

--- play_a_game_of_rock_paper_scissors_in_python.py
+++ play_a_game_of_rock_paper_scissors_in_python.py
@@ -2,3 +2,11 @@ import random
 
 options = ("rock", "paper", "scissors")
 running = True
+
+while running:
+
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter your choice (rock, paper, scissors): ")
