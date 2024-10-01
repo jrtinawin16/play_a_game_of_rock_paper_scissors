3f3323cc66f650f753745a7f4b7bf6fe238ce7b1
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-10-01 17:36:22 +0800

    Accomplished winning and draw conditions of the game

--- play_a_game_of_rock_paper_scissors_in_python.py
+++ play_a_game_of_rock_paper_scissors_in_python.py
@@ -10,3 +10,15 @@ while running:
 
     while player not in options:
         player = input("Enter your choice (rock, paper, scissors): ")
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("It's a tie!")
+    elif player == "rock" and computer == "scissors":
+        print("You win!")
+    elif player == "paper" and computer == "rock":
+        print("You win!")
+    elif player == "scissors" and computer == "paper":
+        print("You win!")
